2faf15a6284dec50044238cea35b86462b1c5b72
nicolas-thomazini <nicolasfrezarimt@gmail.com>
2025-04-11 17:00:33 -0300

    refactor(airflow): update DAG Spark

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -6,7 +6,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 dag = DAG(
     dag_id = "sparking_flow",
     default_args = {
-        "owner": "Yusuf Ganiyu",
+        "owner": "Nicolas Thomazini",
         "start_date": airflow.utils.dates.days_ago(1)
     },
 
@@ -26,10 +26,25 @@ python_job = SparkSubmitOperator(
     dag=dag
 )
 
+scala_job = SparkSubmitOperator(
+    task_id="scala_job",
+    conn_id="spark-conn",
+    application="jobs/scala/target/scala-2.12/word-count_2.12-0.1.jar",
+    dag=dag
+)
+
+java_job = SparkSubmitOperator(
+    task_id="java_job",
+    conn_id="spark-conn",
+    application="jobs/java/spark-job/target/spark-job-1.0-SNAPSHOT.jar",
+    java_class="com.airscholar.spark.WordCountJob",
+    dag=dag
+)
+
 end = PythonOperator(
     task_id="end",
     python_callable= lambda: print("Jobs completed successfully"),
     dag=dag
 )
 
-start >> python_job >> end
+start >> [python_job, scala_job, java_job] >> end
